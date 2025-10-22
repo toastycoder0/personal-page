8bb8b9dfcff51d01e6ba8dbd072580d490290972
toastycoder0 <re-jos2002@proton.me>
2025-10-22 02:01:41 -0600

    feat: migrated tailwind to newest version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwind()]
+  }
 })
